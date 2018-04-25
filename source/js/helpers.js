14939292c3b5541844fb3de33ed713c016065c13
Anthony Veaudry <anthony@veaudry.pro>
2018-04-25 18:50:41 +0300

    reorganize js
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

---
+++ source/js/helpers.js
@@ -0,0 +1,13 @@
+//converts date format from "2014-12-11 14:35:49" into 11/12/14
+function dateConverter(date){ 
+  const splitDate = date.split("-");
+  const year = splitDate[0].substring(2);
+  const month = splitDate[1];
+  const splitDay = splitDate[2].split(" ");
+  const day = splitDay[0];
+  return day + "/" + month + "/" + year;
+}
+
+function toTitleCase(str){
+  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,3 @@
-//converts date format from "2014-12-11 14:35:49" into 11/12/14
-function dateConverter(date){ 
-  const splitDate = date.split("-");
-  const year = splitDate[0].substring(2);
-  const month = splitDate[1];
-  const splitDay = splitDate[2].split(" ");
-  const day = splitDay[0];
-  return day + "/" + month + "/" + year;
-}
-
-function toTitleCase(str){
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
 // $( document ).ready(function() {
 
 const usersToMake = 12;
