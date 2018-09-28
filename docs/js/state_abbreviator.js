bd73f98ebe263b38a1d619e74d8c1fcb20220c85
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-28 10:56:04 +0300

    re build
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- docs/js/state_abbreviator.js
+++ docs/js/state_abbreviator.js
@@ -1,4 +1,111 @@
-function convertStateRegion(a,e){void 0===e&&(e=2<a.length?"TO_ABBREVIATED":"TO_NAME");var i,n=[["Alberta","AB"],["British Columbia","BC"],["Manitoba","MB"],["New Brunswick","NB"],["Newfoundland","NF"],["Northwest Territory","NT"],["Nova Scotia","NS"],["Nunavut","NU"],["Ontario","ON"],["Prince Edward Island","PE"],["Quebec","QC"],["Saskatchewan","SK"],["Yukon","YT"]],o=[["Alabama","AL"],["Alaska","AK"],["American Samoa","AS"],["Arizona","AZ"],["Arkansas","AR"],["Armed Forces Americas","AA"],["Armed Forces Europe","AE"],["Armed Forces Pacific","AP"],["California","CA"],["Colorado","CO"],["Connecticut","CT"],["Delaware","DE"],["District Of Columbia","DC"],["Florida","FL"],["Georgia","GA"],["Guam","GU"],["Hawaii","HI"],["Idaho","ID"],["Illinois","IL"],["Indiana","IN"],["Iowa","IA"],["Kansas","KS"],["Kentucky","KY"],["Louisiana","LA"],["Maine","ME"],["Marshall Islands","MH"],["Maryland","MD"],["Massachusetts","MA"],["Michigan","MI"],["Minnesota","MN"],["Mississippi","MS"],["Missouri","MO"],["Montana","MT"],["Nebraska","NE"],["Nevada","NV"],["New Hampshire","NH"],["New Jersey","NJ"],["New Mexico","NM"],["New York","NY"],["North Carolina","NC"],["North Dakota","ND"],["Northern Mariana Islands","NP"],["Ohio","OH"],["Oklahoma","OK"],["Oregon","OR"],["Pennsylvania","PA"],["Puerto Rico","PR"],["Rhode Island","RI"],["South Carolina","SC"],["South Dakota","SD"],["Tennessee","TN"],["Texas","TX"],["US Virgin Islands","VI"],["Utah","UT"],["Vermont","VT"],["Virginia","VA"],["Washington","WA"],["West Virginia","WV"],["Wisconsin","WI"],["Wyoming","WY"]].concat(n),r;
-// So happy that Canada and the US have distinct abbreviations
-// Reusable loop variable
-if("TO_ABBREVIATED"===e){for(a=a.replace(/\w\S*/g,function(a){return a.charAt(0).toUpperCase()+a.substr(1).toLowerCase()}),r=0;r<o.length;r++)if(o[r][0]===a)return o[r][1]}else if("TO_NAME"===e)for(a=a.toUpperCase(),r=0;r<o.length;r++)if(o[r][1]===a)return o[r][0]}
+function convertStateRegion(input, to) {
+
+  if (to === undefined) {
+    if (input.length > 2) {
+      to = "TO_ABBREVIATED";
+    } else {
+      to = "TO_NAME";
+    }
+  }
+
+  var states = [
+    ["Alabama", "AL"],
+    ["Alaska", "AK"],
+    ["American Samoa", "AS"],
+    ["Arizona", "AZ"],
+    ["Arkansas", "AR"],
+    ["Armed Forces Americas", "AA"],
+    ["Armed Forces Europe", "AE"],
+    ["Armed Forces Pacific", "AP"],
+    ["California", "CA"],
+    ["Colorado", "CO"],
+    ["Connecticut", "CT"],
+    ["Delaware", "DE"],
+    ["District Of Columbia", "DC"],
+    ["Florida", "FL"],
+    ["Georgia", "GA"],
+    ["Guam", "GU"],
+    ["Hawaii", "HI"],
+    ["Idaho", "ID"],
+    ["Illinois", "IL"],
+    ["Indiana", "IN"],
+    ["Iowa", "IA"],
+    ["Kansas", "KS"],
+    ["Kentucky", "KY"],
+    ["Louisiana", "LA"],
+    ["Maine", "ME"],
+    ["Marshall Islands", "MH"],
+    ["Maryland", "MD"],
+    ["Massachusetts", "MA"],
+    ["Michigan", "MI"],
+    ["Minnesota", "MN"],
+    ["Mississippi", "MS"],
+    ["Missouri", "MO"],
+    ["Montana", "MT"],
+    ["Nebraska", "NE"],
+    ["Nevada", "NV"],
+    ["New Hampshire", "NH"],
+    ["New Jersey", "NJ"],
+    ["New Mexico", "NM"],
+    ["New York", "NY"],
+    ["North Carolina", "NC"],
+    ["North Dakota", "ND"],
+    ["Northern Mariana Islands", "NP"],
+    ["Ohio", "OH"],
+    ["Oklahoma", "OK"],
+    ["Oregon", "OR"],
+    ["Pennsylvania", "PA"],
+    ["Puerto Rico", "PR"],
+    ["Rhode Island", "RI"],
+    ["South Carolina", "SC"],
+    ["South Dakota", "SD"],
+    ["Tennessee", "TN"],
+    ["Texas", "TX"],
+    ["US Virgin Islands", "VI"],
+    ["Utah", "UT"],
+    ["Vermont", "VT"],
+    ["Virginia", "VA"],
+    ["Washington", "WA"],
+    ["West Virginia", "WV"],
+    ["Wisconsin", "WI"],
+    ["Wyoming", "WY"]
+  ];
+
+  // So happy that Canada and the US have distinct abbreviations
+  var provinces = [
+    ["Alberta", "AB"],
+    ["British Columbia", "BC"],
+    ["Manitoba", "MB"],
+    ["New Brunswick", "NB"],
+    ["Newfoundland", "NF"],
+    ["Northwest Territory", "NT"],
+    ["Nova Scotia", "NS"],
+    ["Nunavut", "NU"],
+    ["Ontario", "ON"],
+    ["Prince Edward Island", "PE"],
+    ["Quebec", "QC"],
+    ["Saskatchewan", "SK"],
+    ["Yukon", "YT"]
+  ];
+
+  var regions = states.concat(provinces);
+
+  var i; // Reusable loop variable
+  if (to === "TO_ABBREVIATED") {
+    input = input.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+    for (i = 0; i < regions.length; i++) {
+      if (regions[i][0] === input) {
+        return regions[i][1];
+      }
+    }
+  } else if (to === "TO_NAME") {
+    input = input.toUpperCase();
+    for (i = 0; i < regions.length; i++) {
+      if (regions[i][1] === input) {
+        return regions[i][0];
+      }
+    }
+  }
+}
