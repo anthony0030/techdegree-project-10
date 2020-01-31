f1b13af8c6d0d56e30ceb07b01016b37e5dc79e8
Anthony Veaudry <anthony@veaudry.pro>
2020-01-31 16:42:12 +0200

    rebuild
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- source/js/helpers.js
+++ source/js/helpers.js
@@ -1,5 +1,5 @@
 //converts date format from "2014-12-11 14:35:49" into 11/12/14
-function dateConverter(date){ 
+function dateConverter(date){
   const splitDate = date.date.split("-");
   const year = splitDate[0].substring(2);
   const month = splitDate[1];
@@ -9,5 +9,5 @@ function dateConverter(date){
 }
 
 function toTitleCase(str){
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  return str
 }
